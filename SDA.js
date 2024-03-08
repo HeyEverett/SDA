2cd1f14913a304613abd33db86713e110f1dedc1
HeyEverett <jevansjr01@gmail.com>
2024-03-08 13:39:06 -0500

    Big update and #s

--- SDA.js
+++ SDA.js
@@ -18,3 +18,37 @@ function closeMenu() {
     siteNavigation.setAttribute('data-state', "closed");
   }, {once: true})
 }
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: 37.508632619710816, lng: -77.52814874593162 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Beaufont",
+  });
+}
+
+initMap();
+
+function openTab(tabName) {
+  var i, x;
+  x = document.getElementsByClassName("containerTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+} 
